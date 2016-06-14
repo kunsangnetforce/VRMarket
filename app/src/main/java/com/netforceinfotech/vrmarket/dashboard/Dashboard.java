ac38f00a7f18a7df4900775f4eeee4c4b5d993b1
kunsang wangyal <kunwangyal05@gmail.com>
2016-06-14 18:24:01 +0530

    search menu

--- app/src/main/java/com/netforceinfotech/vrmarket/dashboard/Dashboard.java
+++ app/src/main/java/com/netforceinfotech/vrmarket/dashboard/Dashboard.java
@@ -1,23 +1,28 @@
 package com.netforceinfotech.vrmarket.dashboard;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.netforceinfotech.vrmarket.R;
+import com.netforceinfotech.vrmarket.search.SearchActivity;
 
 public class Dashboard extends AppCompatActivity {
 
     private Toolbar toolbar;
     private DashboardFragment dashboardFragment;
+    private FloatingSearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +32,9 @@ public class Dashboard extends AppCompatActivity {
         setupNavigationDrawer();
         setupDashboard();
 
+
     }
+
 
 
     private void setupToolBar() {
@@ -36,6 +43,7 @@ public class Dashboard extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
     private void setupNavigationDrawer() {
         PrimaryDrawerItem home = new PrimaryDrawerItem().withName(R.string.home);
         PrimaryDrawerItem about = new PrimaryDrawerItem().withName(R.string.about);
@@ -56,17 +64,33 @@ public class Dashboard extends AppCompatActivity {
                 })
                 .build();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.search, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.search:
+                startActivity(new Intent(this, SearchActivity.class));
+                overridePendingTransition(R.anim.enter, R.anim.exit);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     private void replaceFragment(Fragment newFragment, String tag) {
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment, newFragment, tag);
         transaction.commit();
     }
+
     private void setupDashboard() {
         if (dashboardFragment == null) {
             dashboardFragment = new DashboardFragment();
